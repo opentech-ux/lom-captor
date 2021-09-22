d6a5ff40f3260bcb6ac29ca563b86aa763842ae4
Rubén Conde <ruben.conde@ux-key.com>
2021-09-22 11:50:46 +0200

    test: Tests changed

--- test/tools.test.js
+++ test/tools.test.js
@@ -2,14 +2,17 @@ import { isBetween } from '../src/modules/tools';
 
 describe('Tools testing', () => {
    describe('isBetween function', () => {
-      it('Should return true because 5 is between 4 and 6', () => {
-         expect(isBetween(5, 4, 6)).toBeTruthy();
+      it('Should return true because 5 is between 1 and 10', () => {
+         expect(isBetween(5, 1, 10)).toBeTruthy();
       });
-      it('Should return true because 5 is between 5 and 6', () => {
-         expect(isBetween(5, 5, 6)).toBeTruthy();
+      it('Should return true because 1 is between 1 and 10', () => {
+         expect(isBetween(1, 1, 10)).toBeTruthy();
       });
-      it('Should return false because 4 is NOT between 5 and 6', () => {
-         expect(isBetween(4, 5, 6)).toBeFalsy();
+      it('Should return true because 10 is between 1 and 10', () => {
+         expect(isBetween(10, 1, 10)).toBeTruthy();
+      });
+      it('Should return false because 15 is NOT between 1 and 10', () => {
+         expect(isBetween(15, 1, 10)).toBeFalsy();
       });
    });
 });
